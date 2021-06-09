e3a86f6b65c656587e1846bf7fd63afd4993ac0c
Kieran Brady <kieran.brady123@gmail.com>
2021-06-09 14:59:50 +0100

    Added a colour system to the map markers that indicates the elevation of each vocano. Green = < 1000m, Orange = 1000m-2999m, Red = 3000m+

--- map1.py
+++ map1.py
@@ -14,12 +14,20 @@ Volcano name:<br>
 Height: %s m
 """
 
+def colour_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el, nam in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nam, nam, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), fill_color=colour_producer(el), color='grey', fill_opacity=0.7))
 
 map.add_child(fg)
 map.save("Map1.html")
