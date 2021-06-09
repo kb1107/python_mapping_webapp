a462d7b8be4fb43a4aebb566bd169099ea58e47a
Kieran Brady <kieran.brady123@gmail.com>
2021-06-09 14:06:00 +0100

    Initial Commit.

---
+++ map1.py
@@ -0,0 +1,17 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi, I am a marker.", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+
+map.save("Map1.html")
