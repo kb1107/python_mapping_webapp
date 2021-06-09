4df0506357d6cb6dc660583b9c6c2e1e1d09d44e
Kieran Brady <kieran.brady123@gmail.com>
2021-06-09 15:32:22 +0100

    Added a layer to the map which indicates the population of each country. Green = < 10000000, Orange = 10000000-19999999, Red = 20000000+

--- map1.py
+++ map1.py
@@ -29,5 +29,9 @@ for lt, ln, el, nam in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nam, nam, el), width=200, height=100)
     fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), fill_color=colour_producer(el), color='grey', fill_opacity=0.7))
 
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+ else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 map.save("Map1.html")
